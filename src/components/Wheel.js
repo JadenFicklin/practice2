1dd1bf55779c899cd3081e7a1f43f892fdaf8112
JadenFicklin <jadendficklin@gmail.com>
2024-06-06 20:39:08 -0600

    added alt property

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -9,29 +9,34 @@ export const Wheel = () => {
     <>
       <div className="w-7/12 mx-auto h-[70vh] mt-[15vh] text-center">
         <h1 className="text-6xl">Simplify. Unify. Amplify.</h1>
-        <h2 className="text-2xl mt-5">
+        <h2 className="mt-5 text-2xl">
           Simplify your process, unify your community, and amplify your impact
           with the full suite of Finalsite School Engagement Solutions.
         </h2>
-        <div className="w-full h-3/4 mt-5 grid place-content-center">
-          <div className=" size-52 relative mx-auto group">
+        <div className="grid w-full mt-5 h-3/4 place-content-center">
+          <div className="relative mx-auto  size-52 group">
             <img
+              alt="wheel section"
               src={logo}
-              className="size-14 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+              className="absolute -translate-x-1/2 -translate-y-1/2 size-14 left-1/2 top-1/2"
             />
             <img
+              alt="wheel section"
               src={blueArrow}
               className="absolute size-52 -top-[90px] left-[90px] -rotate-6 group-hover:opacity-50 duration-300 hover:scale-105"
             />
             <img
+              alt="wheel section"
               src={tealArrow}
               className="absolute size-52 top-[90px] left-[90px] -rotate-6 group-hover:opacity-50 duration-300 hover:scale-105"
             />
             <img
+              alt="wheel section"
               src={redArrow}
               className="absolute size-52 top-[90px] -left-[90px] -rotate-6 group-hover:opacity-50 duration-300 hover:scale-105"
             />
             <img
+              alt="wheel section"
               src={pinkArrow}
               className="absolute size-52 -top-[90px] -left-[90px] -rotate-6 group-hover:opacity-50 duration-300 hover:scale-105"
             />
